b638d91bd99a4775cad9e5b06927cf02b102ed80
CWKSC <CWKSC.person@gmail.com>
2020-03-20 02:22:54 +0800

    Update MyArray and MySort

---
+++ VisualStudioProject/MyLib_Csharp/CommonClass/MyArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib_Csharp.CommonClass
+{
+    class MyArray
+    {
+
+        public static int[] GenerateRandIntArray(int n)
+        {
+            int[] result = new int[n];
+            Random random = new Random();
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = random.Next(int.MinValue, int.MaxValue);
+            }
+            return result;
+        }
+    }
+}
---
+++ VisualStudioProject/MyLib_Csharp/CommonClass/MySort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib_Csharp.CommonClass
+{
+    class MySort
+    {
+        public static T[] BubbleSort<T>(T[] array) where T : IComparable
+        {
+            
+            T[] result = (T[])array.Clone();
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j].CompareTo(array[j + 1]) > 0)
+                    {
+                        T temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static void Test()
+        {
+            int[] testArray = MyArray.GenerateRandIntArray(100);
+        }
+    }
+}
