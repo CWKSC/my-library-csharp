16ef35aa1a5a67d47e4914500d32e7ae2b06e7b5
CWKSC <CWKSC.person@gmail.com>
2020-04-06 01:29:53 +0800

    use partial class to change the folder structure

---
+++ VisualStudioProject/MyLib_Csharp/CommonClass/MyMath/MyMath.Trigonometric.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace MyLib_Csharp.CommonClass
+{
+    public partial class MyMath
+    {
+
+        public static double Sin(double degrees) => Math.Sin(DegreesToRadians(degrees));
+        public static double Cos(double degrees) => Math.Cos(DegreesToRadians(degrees));
+        public static double Tan(double degrees) => Math.Tan(DegreesToRadians(degrees));
+        public static double Atan2(Vector2 point) => Math.Atan2(point.Y, point.X);
+        public static double Atan2(List<double> list) => Math.Atan2(list[1], list[0]);
+
+    }
+}
