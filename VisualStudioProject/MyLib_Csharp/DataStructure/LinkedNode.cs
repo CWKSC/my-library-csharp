a0241ba712919587a0b4470445970e37a7667c45
CWKSC <CWKSC.person@gmail.com>
2020-04-15 13:32:12 +0800

    Update LinkedNode

---
+++ VisualStudioProject/MyLib_Csharp/DataStructure/LinkedNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MyLib_Csharp.DataStructure
+{
+    class LinkedNode<T>
+    {
+        public T data;
+        public LinkedList<LinkedNode<T>> children = new LinkedList<LinkedNode<T>>();
+
+        public LinkedNode(T data) => this.data = data;
+
+        public void AddChild(LinkedNode<T> child) => children.AddLast(child);
+
+        public void RemoveChild(LinkedNode<T> child) => children.Remove(child);
+
+
+
+
+    }
+}
