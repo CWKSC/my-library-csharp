f8d75c550388f5aad68777c537563c2af444fc25
CWKSC <CWKSC.person@gmail.com>
2020-04-22 01:50:01 +0800

    Update RGBA

---
+++ VisualStudioProject/MyLib_Csharp/DataStructure/RGBA.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib_Csharp.DataStructure
+{
+    class RGBA
+    {
+        public int r;
+        public int g;
+        public int b;
+        public int a;
+
+    }
+}
