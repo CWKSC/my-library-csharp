30cadc2685cc9ca2b31d7354f4a6ee36b64a2a4f
CWKSC <CWKSC.person@gmail.com>
2020-04-27 02:16:27 +0800

    Update Rule based

---
+++ VisualStudioProject/MyLib_Csharp/Tool/RuleBasedSystem/RuleBasedSystem.Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib_Csharp.Tool
+{
+    public partial class RuleBasedSystem
+    {
+
+
+        public static void Test()
+        {
+            RuleBasedSystem ruleBasedSystem = new RuleBasedSystem();
+
+
+        }
+
+    }
+}
---
+++ VisualStudioProject/MyLib_Csharp/Tool/RuleBasedSystem/RuleBasedSystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib_Csharp.Tool
+{
+
+    public class Condition
+    {
+        public string name;
+        public Func<bool> condition;
+    }
+
+    public class Conclusions
+    {
+
+    }
+
+    public class Rule
+    {
+        public Condition If;
+        public Condition Then;
+    }
+
+    public partial class RuleBasedSystem
+    {
+        public List<Rule> rules = new List<Rule>();
+
+
+
+    }
+
+
+}
