5af953b64810443c60d14b27d3fb2b4597a6e0e3
CWKSC <CWKSC.person@gmail.com>
2020-05-13 23:22:31 +0800

    Add Monitor

---
+++ VisualStudioProject/MyLib_Csharp/CommonClass/Monitor/Monitor.Test.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib_Csharp.CommonClass
+{
+    public class Monitor
+    {
+
+        public static void Test()
+        {
+            Monitor<int> monitor = new Monitor<int>();
+
+            monitor.IsChange(1).Println();
+            monitor.IsChange(1).Println();
+            monitor.IsChange(1).Println();
+            Console.WriteLine();
+            monitor.IsChange(2).Println(); // value change
+            monitor.IsChange(2).Println();
+            monitor.IsChange(2).Println();
+            Console.WriteLine();
+            monitor.IsChange(3).Println(); // value change
+            monitor.IsChange(3).Println();
+            monitor.IsChange(3).Println();
+
+        }
+
+
+    }
+}
--- VisualStudioProject/MyLib_Csharp/Program.cs
+++ VisualStudioProject/MyLib_Csharp/Program.cs
@@ -14,7 +14,8 @@ namespace MyLib_Csharp
 
         public static void Main(string[] args)
         {
-            MyMath.Integral(0, Math.PI, x => x * Math.Sin(x) / (1 + Math.Pow(Math.Cos(x), 2)), 0.0005).Print();
+            Monitor.Test();
+            //MyMath.Integral(0, Math.PI, x => x * Math.Sin(x) / (1 + Math.Pow(Math.Cos(x), 2)), 0.0005).Print();
         }
     }
 }
