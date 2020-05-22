7dde5e8fc7e07a07ffa23c812786cf81d84a7327
CWKSC <CWKSC.person@gmail.com>
2020-05-23 01:42:01 +0800

    update nothing

--- VisualStudioProject/MyLib_Csharp/Program.cs
+++ VisualStudioProject/MyLib_Csharp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MyLib_Csharp.CommonClass;
 using System;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using static MyLib_Csharp.CommonClass.MyAction;
 using static MyLib_Csharp.CommonClass.MyFunction;
@@ -10,17 +11,14 @@ namespace MyLib_Csharp
     class Program
     {
 
+        public static Func<T, int> GetFunc<T>(Func<T, int> func) => func;
 
-        public static RefFunc<int, RefFunc<int, int>> Foo(ref int input)
-        {
-            return (ref int x) => Foo(ref x);
-        }
+        public static int Work(int input) { return 1; }
 
 
         public static void Main(string[] args)
         {
-
-
+            Delegate @delegate = Delegate.CreateDelegate(typeof(int));
         }
 
     }
