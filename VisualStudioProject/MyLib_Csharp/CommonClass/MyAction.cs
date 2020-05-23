9caa72cc242853fd38264c06ae2093d18a6b9668
CWKSC <CWKSC.person@gmail.com>
2020-05-23 18:13:08 +0800

    Update MyAction, MyFunction

--- VisualStudioProject/MyLib_Csharp/CommonClass/MyAction.cs
+++ VisualStudioProject/MyLib_Csharp/CommonClass/MyAction.cs
@@ -9,15 +9,16 @@ namespace MyLib_Csharp.CommonClass
     {
 
         public delegate void ParamsAction(params object[] args);
-
         public delegate void ParamsAction<IN>(params IN[] args);
-
         public delegate void RefAction<IN>(ref IN arg);
-
         public delegate void OutAction<IN>(out IN arg);
-
         public delegate void InAction<IN>(in IN arg);
 
+
+        public delegate ParamsAction<RecursionParamsAction<T>> RecursionParamsAction<T>(params T[] args);
+        public delegate RefAction<RecursionRefAction<T>> RecursionRefAction<T>(ref T arg);
+        public delegate OutAction<RecursionOutAction<T>> RecursionOutAction<T>(out T arg);
+        public delegate InAction<RecursionInAction<T>> RecursionInAction<T>(in T arg);
 
         public static ParamsAction CallerLayer<T>(params T[] args) => (args2) => BodyLayer(args, args2);
         public static void BodyLayer<T>(T[] args, object[] args2) { /* ... */ }
--- VisualStudioProject/MyLib_Csharp/CommonClass/MyFunction.cs
+++ VisualStudioProject/MyLib_Csharp/CommonClass/MyFunction.cs
@@ -24,6 +24,21 @@ namespace MyLib_Csharp.CommonClass
         public static Func<int, T> ToIntInputFunc<T>(this Func<double, T> f) => x => f(x);
 
 
+        /*
+
+        public static RecursionRefFunc<T> Foo<T>(ref T input)
+        {
+            Console.WriteLine(input);
+            return (ref T t) => { Foo(ref t); return Foo; };
+        }
+
+        public static void Main(string[] args)
+        {
+            int x1 = 1, x2 = 2, x3 = 3, x4 = 4, x5 = 5;
+            Foo(ref x1)(ref x2)(ref x3)(ref x4)(ref x5);
+        }
+
+        */
 
 
 
