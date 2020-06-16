0ca73acfdf4d6a9408e1f6706c47e641b4cf1c59
CWKSC <CWKSC.person@gmail.com>
2020-06-16 22:41:34 +0800

    Update

--- VisualStudioProject/MyLib_Csharp/Program.cs
+++ VisualStudioProject/MyLib_Csharp/Program.cs
@@ -1,17 +1,19 @@
 ﻿using MyLib_Csharp.CommonClass.MyType;
 using System;
-
+using System.Linq.Expressions;
 
 namespace MyLib_Csharp
 {
     class Program
     {
 
+
         public static void Main(string[] args)
         {
             MyAction.Test();
             //MultipleInheritance.Test();
         }
+
 
     }
 
