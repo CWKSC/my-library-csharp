30ca2d64afc67621bd686ff8969174b09388f6ff
CWKSC <CWKSC.person@gmail.com>
2020-07-19 21:18:22 +0800

    Update SumOf_ProductOf, i stop in here a little time, because i think it can abstruct to the fold programming pattern like fold in Haskell

--- VisualStudioProject/MyLib_Csharp_Beta/Math/SumOf_ProductOf/SumOf_ProductOf.cs
+++ VisualStudioProject/MyLib_Csharp_Beta/Math/SumOf_ProductOf/SumOf_ProductOf.cs
@@ -9,12 +9,28 @@ namespace MyLib_Csharp_Beta.Math
     public static partial class SumOf_ProductOf
     {
 
-        public static double SumOf(this (int start, int end) args, MyFunc<int, double> f, MyFunc<int, bool> condition = null)
+        public static double SumOf(this (int start, int end, int step) args, MyFunc<int, double> f, MyFunc<int, bool> condition = null)
         {
             double sum = 0;
             args.Loop(i => sum += f.Invoke(i), condition);
             return sum;
         }
+
+        public static double SumOf(this (int start, int end) args, MyFunc<int, double> f, MyFunc<int, bool> condition = null) =>
+            (args.start, args.end, 1).SumOf(f, condition);
+
+
+        public static double ProductOf(this (int start, int end, int step) args, MyFunc<int, double> f, MyFunc<int, bool> condition = null)
+        {
+            double product = 1;
+            args.Loop(i => product *= f.Invoke(i), condition);
+            return product;
+        }
+
+        public static double ProductOf(this (int start, int end) args, MyFunc<int, double> f, MyFunc<int, bool> condition = null) =>
+            (args.start, args.end, 1).ProductOf(f, condition);
+
+
 
     }
 }
