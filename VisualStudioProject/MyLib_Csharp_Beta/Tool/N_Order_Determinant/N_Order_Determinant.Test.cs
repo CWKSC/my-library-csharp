b6efe758c00008200d1f2c1b5af9da941d1cd419
CWKSC <CWKSC.person@gmail.com>
2020-10-11 22:26:40 +0800

    Update N_Order_Determinant base file

---
+++ VisualStudioProject/MyLib_Csharp_Beta/Tool/N_Order_Determinant/N_Order_Determinant.Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib_Csharp_Beta.Tool
+{
+    public partial class N_Order_Determinant
+    {
+
+        public static void Test()
+        {
+
+        }
+
+    }
+}
---
+++ VisualStudioProject/MyLib_Csharp_Beta/Tool/N_Order_Determinant/N_Order_Determinant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyLib_Csharp_Beta.Tool
+{
+    public partial class N_Order_Determinant
+    {
+
+
+        public static void Generate()
+        {
+
+
+        }
+
+    }
+}
